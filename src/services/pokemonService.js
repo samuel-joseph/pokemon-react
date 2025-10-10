40a10b17e92afef408a0c3bed033d54e27be7a64
arcanlte <pinangayjoel@gmail.com>
2025-10-10 12:07:28 -0400

    updated to es6 arrow functionality

--- src/services/pokemonService.js
+++ src/services/pokemonService.js
@@ -1,54 +1,54 @@
 const API_URL = import.meta.env.VITE_API_URL;
 // const API_URL = "http://localhost:3000";
 
-export async function fetchRegionPokemons(region) {
+export const fetchRegionPokemons = async (region) => {
   const response = await fetch(`${API_URL}/api/region/${region}`);
   if (!response.ok) {
     throw new Error("Failed to fetch Pokémons for the specified region");
   }
   const data = await response.json();
   return data.pokemons;
-}
+};
 
-export async function fetchPokemons() {
+export const fetchPokemons = async () => {
   const response = await fetch(`${API_URL}/api/pokemon`);
   if (!response.ok) {
     throw new Error("Failed to fetch Pokémons");
   }
   const data = await response.json();
   return data.pokemons;
-}
+};
 
 // Fetch all NPCs
-export async function fetchAllNpc() {
+export const fetchAllNpc = async () => {
   const response = await fetch(`${API_URL}/api/npc`);
   if (!response.ok) {
     throw new Error("Failed to fetch NPCs");
   }
   const data = await response.json();
   return data; // returns an array of regions with gymLeaders and Pokémon
-}
+};
 
 // Fetch a single NPC by ID or name
-export async function fetchNpc(id) {
+export const fetchNpc = async (id) => {
   const response = await fetch(`${API_URL}/api/npc/${id}`);
   if (!response.ok) {
     throw new Error(`Failed to fetch NPC with id ${id}`);
   }
   const data = await response.json();
   return data; // returns a single NPC object
-}
+};
 
-export async function getRecord() {
+export const getRecord = async () => {
   const response = await fetch(`${API_URL}/api/leaderboard`);
   if (!response.ok) {
     throw new Error(`Failed to fetch NPC with id ${id}`);
   }
   const data = await response.json();
   return data;
-}
+};
 
-export async function addRecord({ name, record }) {
+export const addRecord = async ({ name, record }) => {
   try {
     const res = await fetch(`${API_URL}/api/leaderboard`, {
       method: "POST",
@@ -66,9 +66,9 @@ export async function addRecord({ name, record }) {
   } catch (err) {
     console.error("Error adding player:", err);
   }
-}
+};
 
-export async function updateRecord({ name, record }) {
+export const updateRecord = async ({ name, record }) => {
   try {
     const res = await fetch(`${API_URL}/leaderboard/${name}`, {
       method: "PUT",
@@ -87,4 +87,4 @@ export async function updateRecord({ name, record }) {
     console.error("Error updating player:", err);
     throw err;
   }
-}
+};
